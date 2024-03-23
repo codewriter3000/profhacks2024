a36b55cd8db0aa2dd45904a0c9ff0e9eb7d3807c
grumpyCapuchins <jenkinsaustin708@gmail.com>
2024-03-23 19:25:15 -0400

    removed unnecessary file and added dropdown code to topbar.jsx

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -2,7 +2,11 @@ export default () => {
     return (
         <div className="p-6 bg-slate-200/30 rounded-3xl">
             <h1 className="text-3xl">New Jersey Shoretown Profiles</h1>
-            
+            <select className="Counties rounded-3x1">
+                <optgroup label="Counties">
+                    <option value="Cape May font-sans">Cape May</option>
+                </optgroup>
+            </select>
         </div>
     )
 }
