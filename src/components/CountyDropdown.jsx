d64a65f81b0b083dab371d592d07c95cce221e93
grumpyCapuchins <jenkinsaustin708@gmail.com>
2024-03-24 10:12:34 -0400

    Slight changes to make the main page a little more pretty

--- src/components/CountyDropdown.jsx
+++ src/components/CountyDropdown.jsx
@@ -6,9 +6,9 @@ export default ({ counties, municipalities, setValue }) => {
 
     return (
         <div className="flex flex-row justify-center items-center">
-            <label htmlFor="counties">Counties</label>
-            <select name="counties" className="rounded-3xl" onChange={handleCountyChange}>
-                <option value={0}>Please select an option</option>
+            <label className="whitespace-pre" htmlFor="counties">Select a shore County: </label>
+            <select name="counties" className="p-2 bg-white/30 px-4 py-1 rounded-3xl" onChange={handleCountyChange}>
+                <option value={0} selected disabled>Please select an option</option>
                 {counties.map(county => {
                     return (
                         <option key={county.county_code} value={county.county_code}>{county.county_name}</option>
--- src/components/MunicipalityTable.jsx
+++ src/components/MunicipalityTable.jsx
@@ -6,17 +6,17 @@ export default ({ municipalities }) => {
         <div className={"mt-6 p-6 bg-slate-200/30 rounded-3xl"}>
             {!municipalities ? <div>Please select a county</div> :
                 <table className="table w-full text-sm">
-                    <thead className="text-xs bg-gray-50/30">
+                    <thead className="text-xs bg-gray-50/30 border">
                         <tr>
-                            <th>Town</th>
-                            <th>Actions</th>
+                            <th className='p-6 text-2xl border-r'>Municipality</th>
+                            <th className='text-2xl'>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
                         {municipalities.map(muni => {
                             return (
-                                <tr key={muni.municipality_code} className="bg-slate-100/30 border-b">
-                                    <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
+                                <tr key={muni.municipality_code} className="bg-slate-100/30 border">
+                                    <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap border-r">
                                         {muni.municipality_name}
                                     </th>
                                     <th className="px-6 py-4">
