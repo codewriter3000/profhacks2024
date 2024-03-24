6081512ddbbef697583194140d95efe7ef90292a
grumpyCapuchins <jenkinsaustin708@gmail.com>
2024-03-24 10:21:47 -0400

    added hover animation for view button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 export default (props) => {
     return (
-        <button className="border rounded-lg bg-white/30 px-4 py-1">{props.children}</button>
+        <button className="transition ease-in-out delay-150 border rounded-lg bg-white/30 px-4 py-1 hover:-translate-y-1 hover:scale-110 hover:bg-white/60 duration-300">{props.children}</button>
     )
 }
