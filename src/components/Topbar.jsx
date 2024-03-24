ea2d99decb922c7242f6343255fdd82373efa58b
grumpyCapuchins <jenkinsaustin708@gmail.com>
2024-03-23 20:35:39 -0400

    Organized and Prettied up the dropdowns

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -3,19 +3,24 @@ export default () => {
         <div className="p-6 bg-slate-200/30 rounded-3xl">
             <h1 className="text-center text-3xl">New Jersey Shoretown Profiles</h1>
             <br></br>
-            <label for="Counties">Choose a County: </label>
-            <select className="p-2 bg-slate-200/30 rounded-3xl">
-                <optgroup label="Counties">
-                    <option value="Cape May font-sans">Cape May</option>
-                </optgroup>
-            </select>
-            
-            <label for="Shore Towns">Choose a Shore Town: </label>
-            <select className="p-2 bg-slate-200/30 rounded-3xl">
-                <optgroup label="Shore Towns">
-                    <option value="Wildwood font-sans">Wildwood</option>
-                </optgroup>
-            </select>
+            <div class="flex">
+                <div class="p-4 flex-1 w-64 rounded-lg flex items-center justify-center bg-slate-500/30 shadow-lg">
+                    <select className="text-emerald-600 p-2 bg-slate-200/30 rounded-3xl">
+                        <optgroup label="Counties">
+                            <option selected disabled>Choose a County</option>
+                            <option value="Cape May font-sans">Cape May</option>
+                        </optgroup>
+                    </select>
+                </div>
+                <div class="p-4 flex-1 w-32 rounded-lg flex items-center justify-center bg-slate-500/30 shadow-lg">
+                    <select className="text-emerald-600 p-2 bg-slate-200/30 rounded-3xl">
+                        <optgroup className="" label="Shore Towns">
+                            <option selected disabled> Choose a Shore Town</option>
+                            <option className="" value="Wildwood font-sans">Wildwood</option>
+                        </optgroup>
+                    </select>
+                </div>
+            </div>
         </div>
     )
 }
